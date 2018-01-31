ad68a4c053da34d44b5a4829eb8afea075b0b600
Jakob Schiotz <schiotz@fysik.dtu.dk>
2018-01-31 12:26:30 +0100

    Fix compilation on Mac.
    This works around a bug in setuptools, where the include_dirs argument to
    setup() is ignored if some configuration file was installed together with
    Python.  On a Mac, this configuration file is present.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
+
+ext_modules=[
+    Extension(
+        "stm.rmsd.qcp",
+        sources=["stm/rmsd/qcp.pyx"],
+        include_dirs=[np.get_include()]
+        )
+    ]
+    
 setup(
     name='structural-template-matching',
     version='1.0',
@@ -10,6 +20,5 @@ setup(
     author_email='jamad@fysik.dtu.dk',
     packages=find_packages(),  #same as name
     install_requires=[], #external packages as dependencies
-    ext_modules=cythonize("stm/rmsd/qcp.pyx"),
-    include_dirs=[np.get_include()]
+    ext_modules=cythonize(ext_modules),
 )
